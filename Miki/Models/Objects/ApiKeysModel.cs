d2e4c4bd2b3ea364b71724ac4a9b857700fad004
Mike <veldsinkie.mv@gmail.com>
2017-07-15 18:05:03 +0200

    Add: database tokens

---
+++ Miki/Models/Objects/ApiKeysModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miki.Models.Objects
+{
+    class ApiKeysModel
+    {
+        public string CarbonitexKey { get; set; }
+        public string DiscordBotsOrgKey { get; set; }
+        public string DiscordPwKey { get; set; }
+        public string DiscordToken { get; set; }
+        public string ImgurClientId { get; set; }
+        public string ImgurKey { get; set; }
+        public string PatreonAccessToken { get; set; }
+        public string RocketLeagueKey { get; set; }
+        public string UrbanKey { get; set; }
+    }
+}
