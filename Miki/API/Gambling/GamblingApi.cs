4bbe621edf59459890d37546fe634b89727b08b8
Mike <veldsinkie.mv@gmail.com>
2017-07-15 18:04:46 +0200

    Add: Gambling API (wip)

---
+++ Miki/API/Gambling/GamblingApi.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miki.API.Gambling
+{
+    class GamblingApi
+    {
+
+        
+    }
+
+    class GamblingSettings
+    {
+        public SlotsSettings slotSettings = new SlotsSettings();
+    }
+
+    class SlotsSettings
+    {
+        private List<SlotsItem> items = new List<SlotsItem>();
+
+        public SlotsSettings AddItem(SlotsItem item)
+        {
+            items.Add(item);
+            return this;
+        }
+        public SlotsSettings AddItem(string emoji, float weight)
+        {
+            return AddItem(new SlotsItem(emoji, weight));
+        }
+    }
+
+    class SlotsItem
+    {
+        public SlotsItem(string emoji, float weight)
+        {
+            Emoji = emoji;
+            Weight = weight;
+        }
+
+        public string Emoji { get; set; }
+        public float Weight { get; set; }
+    }
+
+    class SlotsResponse
+    {
+        int MoneyBack { get; set; }
+        bool Win { get; set; }
+    }
+}
