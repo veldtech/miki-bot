04de4a16cba0d54ee1d680ca6aa71e91e2251070
Mike <veldsinkie.mv@gmail.com>
2017-07-22 04:57:53 +0200

    Add: additional utility methods (GetResource, GetInputBool) & Updated random

--- Miki/Utils.cs
+++ Miki/Utils.cs
@@ -1,5 +1,6 @@
 ﻿using Discord;
 using IA.SDK;
+using IA.SDK.Events;
 using IA.SDK.Interfaces;
 using Miki.Languages;
 using System;
@@ -32,9 +33,19 @@ namespace Miki
               time.Seconds + " second" + ((time.Seconds > 1) ? "s" : "") + ".\n";
         }
 
+        public static bool GetInputBool(this string input)
+        {
+            return (input.ToLower() == "yes" || input.ToLower() == "1" || input.ToLower() == "on");
+        }
+
+        public static string GetResource(this EventContext c, string m, params object[] o) => Locale.GetEntity(c.Channel.Id).GetString(m, o);
+
+        public static Locale GetLocale(this IDiscordMessageChannel c) => Locale.GetEntity(c.Id);
+
         public static DateTime MinDbValue => new DateTime(1755, 1, 1, 0, 0, 0);
 
-        public static IDiscordEmbed Embed => new RuntimeEmbed(new EmbedBuilder());
+        public static IDiscordEmbed Embed => RunEmbed;
+        public static RuntimeEmbed RunEmbed => new RuntimeEmbed(new EmbedBuilder());
 
         public static IDiscordEmbed ErrorEmbed(Locale locale, string message)
         {
@@ -53,6 +64,27 @@ namespace Miki
                 Description = message,
                 Color = new IA.SDK.Color(0, 1, 0)
             };
+        }
+    }
+
+    public class MikiRandom
+    {
+        private static readonly Random getrandom = new Random();
+        private static readonly object syncLock = new object();
+
+        public static int GetRandomNumber(int max)
+        {
+            lock (syncLock)
+            {
+                return getrandom.Next(0, max);
+            }
+        }
+        public static int GetRandomNumber(int min, int max)
+        {
+            lock (syncLock)
+            {
+                return getrandom.Next(min, max);
+            }
         }
     }
 }
