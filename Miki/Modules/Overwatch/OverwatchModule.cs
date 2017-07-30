aeec9137ffc5d1ea1f843f7b0410d6c30c35d27b
Mike <veldsinkie.mv@gmail.com>
2017-07-30 02:08:04 +0200

    Update: >overwatchuser (owuser)

--- Miki/Modules/Overwatch/OverwatchModule.cs
+++ Miki/Modules/Overwatch/OverwatchModule.cs
@@ -1,5 +1,6 @@
 ﻿using IA.Events.Attributes;
 using IA.SDK.Events;
+using IA.SDK.Interfaces;
 using Miki.Modules.Overwatch.API;
 using Miki.Modules.Overwatch.Objects;
 using System;
@@ -16,22 +17,108 @@ namespace Miki.Modules.Overwatch
         [Command(Name = "overwatchuser", Aliases = new string[] { "owuser" })]
         public async Task OverwatchStatsAsync(EventContext e)
         {
-            OverwatchUserResponse user = await InternalGetUser(e);
-        }
-
-        public async Task<OverwatchUserResponse> InternalGetUser(EventContext e)
-        {
             string[] arguments = e.arguments.Split(' ');
-
-            string[] toggles = arguments
-                .Where(x => x.StartsWith("-"))
-                .ToArray();
 
             string[] username = arguments
                 .Where(x => x.Contains("#"))
                 .FirstOrDefault()
                 .Split('#');
 
+            string[] toggles = arguments
+                .Where(x => x.StartsWith("-"))
+                .ToArray();
+
+            OverwatchUserResponse user = await InternalGetUser(e, username);
+
+            if(user != null)
+            {
+                bool competitive = toggles.Contains("-c");
+
+                OverwatchRegion region;
+                if (toggles.Contains("-eu"))
+                {
+                    region = user.Europe;
+                }
+                else if(toggles.Contains("-us"))
+                {
+                    region = user.America;
+                }
+                else if (toggles.Contains("-kr"))
+                {
+                    region = user.Korea;
+                }
+                else
+                {
+                    region = GetBestRegion(user, competitive);
+                }
+
+                IDiscordEmbed embed = Utils.Embed
+                    .SetTitle(string.Join("#", username) + "'s Overwatch Profile");
+
+                if (competitive)
+                {
+                    var orderedPlaytime = region.heroes.playtime.competitive.OrderByDescending(x => x.Value);
+
+                    int seconds = (int)Math.Round(orderedPlaytime.First().Value * 60 * 60);
+
+                    await embed
+                        .SetThumbnailUrl(region.stats.competitive.OverallStats.avatar)
+                        .AddInlineField("MMR", region.stats.competitive.OverallStats.tier.ToString() + " (" + region.stats.competitive.OverallStats.comprank.ToString() + ")")
+                        .AddInlineField("KDA ratio", Math.Round(region.stats.competitive.GameStats.eliminations / region.stats.competitive.GameStats.deaths, 2).ToString())
+                        .AddInlineField("Favourite Character", orderedPlaytime.First().Key + " with " + Utils.ToTimeString(seconds))
+                        .AddInlineField("Winrate", (region.stats.competitive.OverallStats.win_rate) + "%")
+                        
+                        .SendToChannel(e.Channel);
+                }
+                else
+                {
+
+                }
+            }
+            else
+            {
+                // couldnt find xd
+            }
+            
+        }
+
+        // worst function ever.
+        public OverwatchRegion GetBestRegion(OverwatchUserResponse u, bool compo)
+        {
+            float? timePlayed = 0f;
+            OverwatchRegion r = null;
+
+            if(u.America != null)
+            {
+                timePlayed = (compo) ? u.America.heroes.playtime.competitive?.Sum(x => x.Value) : u.America.heroes.playtime.quickplay?.Sum(x => x.Value);
+                r = u.America;
+            }
+
+            if(u.Europe != null)
+            {
+                float? t = (compo) ? u.Europe.heroes.playtime.competitive?.Sum(x => x.Value) : u.Europe.heroes.playtime.quickplay?.Sum(x => x.Value);
+                if(t > timePlayed || t == null)
+                {
+                    timePlayed = t;
+                    r = u.Europe;
+                }
+            }
+
+            if (u.Korea != null)
+            {
+                float? t = (compo) ? u.Korea.heroes.playtime.competitive?.Sum(x => x.Value) : u.Korea.heroes.playtime.quickplay?.Sum(x => x.Value);
+                if (t > timePlayed || t == null)
+                {
+                    timePlayed = t;
+                    r = u.Korea;
+                }
+            }
+
+            return r;
+        }
+
+        public async Task<OverwatchUserResponse> InternalGetUser(EventContext e, string[] username)
+        {
             if (username.Length <= 1)
             {
                 // no discriminator
