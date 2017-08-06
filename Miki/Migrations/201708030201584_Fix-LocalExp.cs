fb04ed84e07a92827daa4ad2d10018dff07d4fdf
Mike <veldsinkie.mv@gmail.com>
2017-08-06 21:21:21 +0200

    Add: NpgSql migrations

---
+++ Miki/Migrations/201708030201584_Fix-LocalExp.cs
@@ -0,0 +1,16 @@
+namespace Miki.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class FixLocalExp : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
