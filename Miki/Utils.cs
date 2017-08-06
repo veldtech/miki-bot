d76f75d64360d96a3a960bfc898a239dc7670c44
Mike <veldsinkie.mv@gmail.com>
2017-08-06 21:16:59 +0200

    Rework: random

--- Miki/Utils.cs
+++ Miki/Utils.cs
@@ -5,6 +5,7 @@ using IA.SDK.Interfaces;
 using Miki.Languages;
 using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 
 namespace Miki
 {
@@ -128,24 +129,47 @@ namespace Miki
         }
     }
 
-    public class MikiRandom
+    public class MikiRandom : RandomNumberGenerator
     {
-        private static readonly Random getrandom = new Random();
-        private static readonly object syncLock = new object();
-            
-        public static int GetRandomNumber(int max)
+        private static readonly RandomNumberGenerator rng = new RNGCryptoServiceProvider();
+
+        public static int Next()
         {
-            lock (syncLock)
-            {
-                return getrandom.Next(0, max);
-            }
+            var data = new byte[sizeof(int)];
+            rng.GetBytes(data);
+            return BitConverter.ToInt32(data, 0) & (int.MaxValue - 1);
         }
-        public static int GetRandomNumber(int min, int max)
+
+        public static int Next(int maxValue)
         {
-            lock (syncLock)
+            return Next(0, maxValue);
+        }
+
+        public static int Next(int minValue, int maxValue)
+        {
+            if (minValue > maxValue)
             {
-                return getrandom.Next(min, max);
+                throw new ArgumentOutOfRangeException();
             }
+            return (int)Math.Floor((minValue + ((double)maxValue - minValue) * NextDouble()));
+        }
+
+        public static double NextDouble()
+        {
+            var data = new byte[sizeof(uint)];
+            rng.GetBytes(data);
+            var randUint = BitConverter.ToUInt32(data, 0);
+            return randUint / (uint.MaxValue + 1.0);
+        }
+
+        public override void GetBytes(byte[] data)
+        {
+            rng.GetBytes(data);
+        }
+
+        public override void GetNonZeroBytes(byte[] data)
+        {
+            rng.GetNonZeroBytes(data);
         }
     }
 
