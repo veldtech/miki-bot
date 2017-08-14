c9712c6a4dc5f363c03064beb66a9939baca7dde
Corey Urquhart <CoreyU49@gmail.com>
2017-08-14 19:11:23 +0100

    Add: cry command

--- Miki/Modules/ReactionsModule.cs
+++ Miki/Modules/ReactionsModule.cs
@@ -109,49 +109,66 @@ namespace Miki.Modules
             await em.SendToChannel(e.Channel);
         }
 
-        [Command(Name = "smug")]
-        public async Task SmugAsync(EventContext e)
-        {
-            string[] images = new string[]
-            {
-                "http://i.imgur.com/zUwqrhM.png",
-                "http://i.imgur.com/TYqPh89.jpg",
-                "http://i.imgur.com/xyOSaCt.png",
-                "http://i.imgur.com/gyw0ifl.png",
-                "http://i.imgur.com/kk0xvtx.png",
-                "http://i.imgur.com/UIuyUne.jpg",
-                "http://i.imgur.com/9zgIjY1.jpg",
-                "http://i.imgur.com/Ku1ONAD.jpg",
-                "http://i.imgur.com/7lB5bRT.jpg",
-                "http://i.imgur.com/BoVHipF.jpg",
-                "http://i.imgur.com/vN48mwz.png",
-                "http://i.imgur.com/fGI4zLe.jpg",
-                "http://i.imgur.com/Gc4gmwQ.jpg",
-                "http://i.imgur.com/JMrmKt7.jpg",
-                "http://i.imgur.com/a7sbJz2.jpg",
-                "http://i.imgur.com/NebmjhR.png",
-                "http://i.imgur.com/5ccbrFI.png",
-                "http://i.imgur.com/XJL4Vmo.jpg",
-                "http://i.imgur.com/eg0q1ez.png",
-                "http://i.imgur.com/JJFxxmA.jpg",
-                "http://i.imgur.com/2cTDF3b.jpg",
-                "http://i.imgur.com/Xc0Duqv.png",
-                "http://i.imgur.com/YgMdPkd.jpg",
-                "http://i.imgur.com/BvAv6an.jpg",
-                "http://i.imgur.com/KRLP5JT.jpg",
-                "http://i.imgur.com/yXcsCK3.jpg",
-                "http://i.imgur.com/QXG56kG.png",
-                "http://i.imgur.com/OFBz1YJ.png",
-                "http://i.imgur.com/9ulVckY.png",
-                "http://i.imgur.com/VLXeSJK.png",
-                "http://i.imgur.com/baiMBP6.png"
-            };
+		[Command( Name = "smug" )]
+		public async Task SmugAsync( EventContext e )
+		{
+			string[] images = new string[]
+			{
+				"http://i.imgur.com/zUwqrhM.png",
+				"http://i.imgur.com/TYqPh89.jpg",
+				"http://i.imgur.com/xyOSaCt.png",
+				"http://i.imgur.com/gyw0ifl.png",
+				"http://i.imgur.com/kk0xvtx.png",
+				"http://i.imgur.com/UIuyUne.jpg",
+				"http://i.imgur.com/9zgIjY1.jpg",
+				"http://i.imgur.com/Ku1ONAD.jpg",
+				"http://i.imgur.com/7lB5bRT.jpg",
+				"http://i.imgur.com/BoVHipF.jpg",
+				"http://i.imgur.com/vN48mwz.png",
+				"http://i.imgur.com/fGI4zLe.jpg",
+				"http://i.imgur.com/Gc4gmwQ.jpg",
+				"http://i.imgur.com/JMrmKt7.jpg",
+				"http://i.imgur.com/a7sbJz2.jpg",
+				"http://i.imgur.com/NebmjhR.png",
+				"http://i.imgur.com/5ccbrFI.png",
+				"http://i.imgur.com/XJL4Vmo.jpg",
+				"http://i.imgur.com/eg0q1ez.png",
+				"http://i.imgur.com/JJFxxmA.jpg",
+				"http://i.imgur.com/2cTDF3b.jpg",
+				"http://i.imgur.com/Xc0Duqv.png",
+				"http://i.imgur.com/YgMdPkd.jpg",
+				"http://i.imgur.com/BvAv6an.jpg",
+				"http://i.imgur.com/KRLP5JT.jpg",
+				"http://i.imgur.com/yXcsCK3.jpg",
+				"http://i.imgur.com/QXG56kG.png",
+				"http://i.imgur.com/OFBz1YJ.png",
+				"http://i.imgur.com/9ulVckY.png",
+				"http://i.imgur.com/VLXeSJK.png",
+				"http://i.imgur.com/baiMBP6.png"
+			};
 
-            RuntimeEmbed em = new RuntimeEmbed(new Discord.EmbedBuilder())
-            {
-                ImageUrl = images[Global.random.Next(0, images.Length)]
-            };
-            await em.SendToChannel(e.Channel);
-        }
-    }
+			RuntimeEmbed em = new RuntimeEmbed( new Discord.EmbedBuilder() )
+			{
+				ImageUrl = images[Global.random.Next( 0, images.Length )]
+			};
+			await em.SendToChannel( e.Channel );
+		}
+
+		[Command( Name = "cry" )]
+		public async Task CryAsync( EventContext e )
+		{
+			string[] images = new string[]
+			{
+				"http://i.imgur.com/TTUBf2r.gif",
+				"http://i.imgur.com/o66oQyX.png",
+				"http://i.imgur.com/6AP78bD.png"
+			};
+
+			RuntimeEmbed em = new RuntimeEmbed( new Discord.EmbedBuilder() )
+			{
+				ImageUrl = images[Global.random.Next( 0, images.Length )]
+			};
+			await em.SendToChannel( e.Channel );
+		}
+	}
 }
