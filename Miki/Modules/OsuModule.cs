c98e7478f6c21a524505b5f86983ffd4430fb1b8
Corey Urquhart <CoreyU49@gmail.com>
2017-08-19 18:43:17 +0100

    Repair: broken modules

--- Miki/Modules/OsuModule.cs
+++ Miki/Modules/OsuModule.cs
@@ -1,90 +1,69 @@
-using IA;
-using IA.Events.Attributes;
-using IA.Extension;
 using IA.SDK;
 using IA.SDK.Events;
-using IA.SDK.Interfaces;
-using Imgur.API.Authentication.Impl;
-using Imgur.API.Endpoints.Impl;
-using Imgur.API.Models;
-using Miki.Accounts.Achievements;
-using Miki.Accounts.Achievements.Objects;
-using Miki.API;
-using Miki.Languages;
-using Miki.Models;
-using Miki.Objects;
-using NCalc;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
+using IA.Events.Attributes;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace Miki.Modules.OsuModule
+namespace Miki.Modules
 {
-    [Module("Osu")]
-    public class OsuModule
-    {
-        public OsuModule(RuntimeModule module)
+	[Module( Name = "Osu" )]
+	public class OsuModule
+	{
+		[Command( Name = "osu" )]
+		public async Task SendOsuSignatureAsync( EventContext e )
+		{
+			using( WebClient webClient = new WebClient() )
+			{
+				byte[] data = webClient.DownloadData( "http://lemmmy.pw/osusig/sig.php?colour=pink&uname=" + e.arguments + "&countryrank" );
 
-        [Command(Name = "osu")]
-        public async Task SendOsuSignatureAsync(EventContext e)
-        {
-            using (WebClient webClient = new WebClient())
-            {
-                byte[] data = webClient.DownloadData("http://lemmmy.pw/osusig/sig.php?colour=pink&uname=" + e.arguments + "&countryrank");
+				using( MemoryStream mem = new MemoryStream( data ) )
+				{
+					await e.Channel.SendFileAsync( mem, $"sig.png" );
+				}
+			}
+		}
 
-                using (MemoryStream mem = new MemoryStream(data))
-                {
-                    await e.Channel.SendFileAsync(mem, $"sig.png");
-                }
-            }
-        }
+		[Command( Name = "ctb" )]
+		public async Task SendCatchTheBeatSignatureAsync( EventContext e )
+		{
+			using( WebClient webClient = new WebClient() )
+			{
+				byte[] data = webClient.DownloadData( "http://lemmmy.pw/osusig/sig.php?colour=pink&uname=" + e.arguments + "&mode=2&countryrank" );
 
-        [Command(Name = "ctb")]
-        public async Task SendCatchTheBeatSignatureAsync(EventContext e)
-        {
-            using (WebClient webClient = new WebClient())
-            {
-                byte[] data = webClient.DownloadData("http://lemmmy.pw/osusig/sig.php?colour=pink&uname=" + e.arguments + "&mode=2&countryrank");
+				using( MemoryStream mem = new MemoryStream( data ) )
+				{
+					await e.Channel.SendFileAsync( mem, $"{e.arguments}.png" );
+				}
+			}
+		}
 
-                using (MemoryStream mem = new MemoryStream(data))
-                {
-                    await e.Channel.SendFileAsync(mem, $"{e.arguments}.png");
-                }
-            }
-        }
+		[Command( Name = "mania" )]
+		public async Task SendManiaSignatureAsync( EventContext e )
+		{
+			using( WebClient webClient = new WebClient() )
+			{
+				byte[] data = webClient.DownloadData( "http://lemmmy.pw/osusig/sig.php?colour=pink&uname=" + e.arguments + "&mode=3&countryrank" );
 
-        [Command(Name = "mania")]
-        public async Task SendManiaSignatureAsync(EventContext e)
-        {
-            using (WebClient webClient = new WebClient())
-            {
-                byte[] data = webClient.DownloadData("http://lemmmy.pw/osusig/sig.php?colour=pink&uname=" + e.arguments + "&mode=3&countryrank");
+				using( MemoryStream mem = new MemoryStream( data ) )
+				{
+					await e.Channel.SendFileAsync( mem, $"sig.png" );
+				}
+			}
+		}
 
-                using (MemoryStream mem = new MemoryStream(data))
-                {
-                    await e.Channel.SendFileAsync(mem, $"sig.png");
-                }
-            }
-        }
+		[Command( Name = "taiko" )]
+		public async Task SendTaikoSignatureAsync( EventContext e )
+		{
+			using( WebClient webClient = new WebClient() )
+			{
+				byte[] data = webClient.DownloadData( "http://lemmmy.pw/osusig/sig.php?colour=pink&uname=" + e.arguments + "&mode=1&countryrank" );
 
-        [Command(Name = "taiko")]
-        public async Task SendTaikoSignatureAsync(EventContext e)
-        {
-            using (WebClient webClient = new WebClient())
-            {
-                byte[] data = webClient.DownloadData("http://lemmmy.pw/osusig/sig.php?colour=pink&uname=" + e.arguments + "&mode=1&countryrank");
-
-                using (MemoryStream mem = new MemoryStream(data))
-                {
-                    await e.Channel.SendFileAsync(mem, $"sig.png");
-                }
-            }
-        }
-    }
+				using( MemoryStream mem = new MemoryStream( data ) )
+				{
+					await e.Channel.SendFileAsync( mem, $"sig.png" );
+				}
+			}
+		}
+	}
 }
