d717b7b1e15a8d06322f123696ad911cbf024d7c
Veld <velddev@gmail.com>
2018-04-09 04:02:25 +0200

    Change: value to debug (have to make this a config value)

--- Miki/Models/MikiContext.cs
+++ Miki/Models/MikiContext.cs
@@ -12,7 +12,7 @@ namespace Miki.Models
     public class MikiContext : DbContext
     {
 		public static readonly LoggerFactory logger
-			= new LoggerFactory(new[] { new ConsoleLoggerProvider((_, __) => __ > LogLevel.Information, true) });
+			= new LoggerFactory(new[] { new ConsoleLoggerProvider((_, __) => __ > LogLevel.Debug, true) });
 
 		public DbSet<Achievement> Achievements { get; set; }
         public DbSet<CommandUsage> CommandUsages { get; set; }
