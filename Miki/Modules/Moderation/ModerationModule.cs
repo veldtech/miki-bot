26cf4fa8fc9dd86f55f13d856cd740125399b301
Veld <velddev@gmail.com>
2018-11-09 01:32:39 +0100

    big oof

--- Miki/Modules/Moderation/ModerationModule.cs
+++ Miki/Modules/Moderation/ModerationModule.cs
@@ -6,22 +6,23 @@ using System.Threading.Tasks;
 
 namespace Miki.Modules.Moderation
 {
-	[Module("Moderation")]
-	public class ModerationModule
-	{
-		public ModerationModule(Module m)
-		{
-			m.UserJoinGuild += OnUserJoinGuild;
-		}
+	// todo: reimplmenent with better regex or customizability
+	//[Module("Moderation")]
+	//public class ModerationModule
+	//{
+	//	public ModerationModule(Module m)
+	//	{
+	//		m.UserJoinGuild += OnUserJoinGuild;
+	//	}
 
-		private async Task OnUserJoinGuild(IDiscordGuildUser arg)
-		{
-			var matches = Regex.Matches(arg.Username, "(^|\\s)((https?:\\/\\/)?[\\w-]+(\\.[\\w-]+)+\\.?(:\\d+)?(\\/\\S*)?)");
+	//	private async Task OnUserJoinGuild(IDiscordGuildUser arg)
+	//	{
+	//		var matches = Regex.Matches(arg.Username, "(^|\\s)((https?:\\/\\/)?[\\w-]+(\\.[\\w-]+)+\\.?(:\\d+)?(\\/\\S*)?)");
 
-			if (matches.Count > 0)
-			{
-				await arg.KickAsync("detected url in username.");
-			}
-		}
-	}
+	//		if (matches.Count > 0)
+	//		{
+	//			await arg.KickAsync("detected url in username.");
+	//		}
+	//	}
+	//}
 }
