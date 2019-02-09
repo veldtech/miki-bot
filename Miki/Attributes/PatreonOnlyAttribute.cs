5e55e0bf2f3df8d455383fb0dfa3b0752cc3c7be
Veld <velddev@gmail.com>
2019-02-09 22:12:12 +0100

    Added helpers

---
+++ Miki/Attributes/PatreonOnlyAttribute.cs
@@ -0,0 +1,22 @@
+﻿using Miki.Bot.Models;
+using Miki.Framework.Events;
+using Miki.Framework.Events.Attributes;
+using Miki.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miki
+{
+    public class PatreonOnlyAttribute : CommandRequirementAttribute
+    {
+        public override async Task<bool> CheckAsync(EventContext e)
+        {
+            using (var context = new MikiContext())
+            {
+                return await IsDonator.ForUserAsync(context, e.Author.Id);
+            }
+        }
+    }
+}
