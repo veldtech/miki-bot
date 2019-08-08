bbf97ffd0e1eb9b2229c60cd9e0d217d272aa953
exsersewo <exsersewo@systemexit.co.uk>
2019-08-08 01:34:32 +0100

    Remove Config Global, Comment out AmazonS3Client & Add Constant ENV Vars

--- src/Miki/Global.cs
+++ src/Miki/Global.cs
@@ -9,51 +9,31 @@ namespace Miki
 	/// </summary>
 	public class Global
 	{
-		public static Config Config
-		{
-			get
-			{
-				if (config == null)
-				{
-					if (File.Exists("./miki/settings.json"))
-					{
-						config = JsonConvert.DeserializeObject<Config>(File.ReadAllText("./miki/settings.json"));
-					}
-					else
-					{
-						if (!Directory.Exists("./miki"))
-						{
-							Directory.CreateDirectory("./miki");
-						}
-						config = new Config();
-						File.WriteAllText("./miki/settings.json", JsonConvert.SerializeObject(config, Formatting.Indented));
-					}
-				}
-				return config;
-			}
-		}
-
 		public static AmazonS3Client CdnClient
 		{
 			get
 			{
 				if (cdnClient == null)
 				{
-					cdnClient = new AmazonS3Client(Config.CdnAccessKey, Config.CdnSecretKey, new AmazonS3Config()
+					/*cdnClient = new AmazonS3Client(Config.CdnAccessKey, Config.CdnSecretKey, new AmazonS3Config()
 					{
 						ServiceURL = Config.CdnRegionEndpoint
-					});
+					});*/
 				}
 				return cdnClient;
 			}
 		}
 
-        private static Config config = null;
 		private static AmazonS3Client cdnClient;
 	}
 
 	public class Constants
 	{
 		public const string NotDefined = "$not-defined";
+        public const string ENV_ConStr = "MIKI_CONNSTRING";
+        public const string ENV_MsgWkr = "MIKI_MESSAGEWORKER";
+        public const string ENV_LogLvl = "MIKI_LOGLEVEL";
+        public const string ENV_SelfHost = "MIKI_SELFHOSTED";
+        public const string ENV_ConfId = "MIKI_CONFIGID";
 	}
 }
