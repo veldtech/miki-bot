88e393a316d8fcb360e1fb9928ce1ab9d168c609
exsersewo <exsersewo@systemexit.co.uk>
2019-08-11 00:56:04 +0100

    Remove Globals and move to constants

---
+++ src/Miki/Constants.cs
@@ -0,0 +1,16 @@
+﻿using Amazon.S3;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Miki
+{
+	public class Constants
+	{
+		public const string NotDefined = "$not-defined";
+        public const string ENV_ConStr = "MIKI_CONNSTRING";
+        public const string ENV_MsgWkr = "MIKI_MESSAGEWORKER";
+        public const string ENV_LogLvl = "MIKI_LOGLEVEL";
+        public const string ENV_SelfHost = "MIKI_SELFHOSTED";
+        public const string ENV_ConfId = "MIKI_CONFIGID";
+    }
+}
