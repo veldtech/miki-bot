82fa24ce32c52e74596f15cda276ceb7b96a8ec7
Zenulous <zenvanriel@gmail.com>
2020-09-08 12:27:13 +0200

    Improve Yandere reliability

--- src/Miki/API/Imageboards/YandereProvider.cs
+++ src/Miki/API/Imageboards/YandereProvider.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections.Generic;
 using System.Runtime.Serialization;
 using Miki.API.Imageboards.Objects;
+using Newtonsoft.Json;
 
 namespace Miki.API.Imageboards
 {
     [DataContract]
     public class YandereResponse
     {
-        [DataMember(Name = "posts")]
+        [JsonProperty("posts")]
         public List<YanderePost> Posts { get; set; }
     }
 }
